c9f1a37315dd38b2abd0b80a30f0eb4a4ae6310a
rdentato <rdentato@gmail.com>
2022-10-31 10:24:19 +0100

    Dependencies.

---
+++ deps/src/skp.c
@@ -0,0 +1,11 @@
+/*
+**  (C) by Remo Dentato (rdentato@gmail.com)
+** 
+** This software is distributed under the terms of the MIT license:
+**  https://opensource.org/licenses/MIT
+*/
+
+#ifndef SKP_MAIN
+#define SKP_MAIN
+#endif
+#include "skp.h"
---
+++ deps/src/vrg.c
@@ -0,0 +1,11 @@
+/*
+**  (C) by Remo Dentato (rdentato@gmail.com)
+** 
+** This software is distributed under the terms of the MIT license:
+**  https://opensource.org/licenses/MIT
+*/
+
+#ifndef VRG_MAIN
+#define VRG_MAIN
+#endif
+#include "vrg.h"
