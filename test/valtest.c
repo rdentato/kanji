728fc9fdc0cf630f2ddadcfba3f5058ec082ea85
rdentato <rdentato@gmail.com>
2022-10-31 12:47:41 +0100

    After Design change.

---
+++ test/valtest.c
@@ -0,0 +1,12 @@
+#define VAL_MAIN
+#define DBG_MAIN
+#include "dbg.h"
+#include "val.h"
+
+#include "stdio.h"
+#include "stdlib.h"
+
+int main(int argc, char *argv[])
+{
+   printf("Hello World\n");
+}
